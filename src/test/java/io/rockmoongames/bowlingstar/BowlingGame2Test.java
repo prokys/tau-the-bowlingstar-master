cb3da4aa98183f5ec582d60a322331126964da0d
Lisa <lisa@rockmoongames.io>
2023-04-16 15:30:39 +0200

    Add a fourth test

---
+++ src/test/java/io/rockmoongames/bowlingstar/BowlingGame2Test.java
@@ -0,0 +1,10 @@
+package io.rockmoongames.bowlingstar;
+
+import org.junit.jupiter.api.Test;
+
+public class BowlingGame2Test {
+    @Test
+    public void lisaTestOFour() {
+        //Lisa's test number two
+    }
+}
